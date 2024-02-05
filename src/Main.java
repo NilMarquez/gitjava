4929605c30939540ff4881bc2696a4e0cceb6a90
--nilmarquez <--nilmarquezr@gmail.com>
2024-02-05 12:12:21 +0100

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        //TODO: Main
+
+    }
+    public static void AltaClients() {
+        //TODO: AltaClients
+    }
+
+    public static void AltaVehicles() {
+        //TODO: AltaVehicles
+    }
+
+    public static void AltaEmpleats() {
+        //TODO: AltaEmpleats
+    }
+
+    public static void Reparacions() {
+        //TODO: Reparacions
+    }
+
+    public static void NovaReparacions() {
+        //TODO: NovaReparacions
+    }
+
+    //TODO: Modificar
+}
+/**
+ * Aqui se comentarien els mètodes
+ * Test primer commit
+ */
+
+}
