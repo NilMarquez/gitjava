28df677c73ac17c3c8fdf54a49bee3ac504e2e63
--nilmarquez <--nilmarquezr@gmail.com>
2024-02-05 13:22:45 +0100

    Menu + Metodes

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,60 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        //TODO: Main
+package com.company;
 
+import java.util.Scanner;
+
+public class Main {
+
+    Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.init();
     }
+
+    public void init() {
+        int menuItem = 0;
+        do {
+            System.out.println("TALLER DE REPARACIÓ DE VEHICLES");
+            System.out.println("[1] Donar d’alta nou client");
+            System.out.println("[2] Donar d’alta nou mecànic");
+            System.out.println("[3] Introduir nou vehicle");
+            System.out.println("[4] Crear fitxa de nova reparació");
+            System.out.println("[5] Sortir");
+            System.out.println("Sel·lecciona una opció: ");
+
+            if (input.hasNextInt()) {
+                menuItem = input.nextInt();
+                switch (menuItem) {
+                    case 1:
+                        System.out.println("Has triat donar d’alta nou client....");
+                        AltaClients();
+                        break;
+                    case 2:
+                        System.out.println("Has triat donar d’alta nou mecànic....");
+                        AltaEmpleats();
+                        break;
+                    case 3:
+                        System.out.println("Has triat introduir nou vehicle....");
+                        AltaVehicles();
+                        break;
+                    case 4:
+                        System.out.println("Has triat crear fitxa de nova reparació....");
+                        NovaReparacions();
+                        break;
+                    case 5:
+                        System.out.println("Sortint....");
+                        break;
+                    default:
+                        System.out.println("Opció no vàlida");
+                }
+            } else {
+                System.out.println("Opció no vàlida");
+            }
+            input.nextLine();
+            System.out.println("");
+        } while (menuItem != 5);
+    }
+
     public static void AltaClients() {
         //TODO: AltaClients
     }
@@ -32,5 +82,3 @@ public class Main {
  * Test primer commit
  * TEST NIL FETE
  */
-
-}
