f5cdfe8ddde829064e302b70bd3654034d6fead4
Ivan Vallejo <ivalleciura@gmail.com>
2024-02-12 10:17:00 +0100

    Alta_Vehicles

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,123 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Main {
+
+    Scanner input = new Scanner(System.in);
+
+    public static String[][] vehicles = new String[100][3];
+    public static String[][] clients = new String[100][4];
+    public static int xv = 0;
+
     public static void main(String[] args) {
-        //TODO: Main
+
+        Main main = new Main();
+        main.init();
 
     }
-    public static void AltaClients() {
-        //TODO: AltaClients
+
+
+
+    public void init(){
+        int menuItem = 0;
+        do {
+            System.out.println("TALLER DE REPARACIÓ DE VEHICLES");
+            System.out.println("[1] Donar d’alta nou client");
+            System.out.println("[2] Donar d’alta nou mecànic");
+            System.out.println("[3] Introduir nou vehicle");
+            System.out.println("[4] Crear fitxa de nova reparació");
+            System.out.println("[5] Sortir");
+            System.out.println("Sel·lecciona una opció: ");
+
+            if (input.hasNextInt()){
+                menuItem = input.nextInt();
+                switch (menuItem){
+                    case 1:
+                        System.out.println("Has triat donar d’alta nou client....");
+                        //insert code here
+                        break;
+                    case 2:
+                        System.out.println("Has triat donar d’alta nou mecànic....");
+                        //insert code here
+                        break;
+                    case 3:
+                        System.out.println("Has triat introduir nou vehicle....");
+                        //insert code here
+                        AltaVehicle();
+                        for (int i=0; i<vehicles.length; i++) {
+                            if (vehicles[i][0] != null) {
+                                for (int j = 0; j < vehicles[i].length; j++) {
+                                    System.out.print(vehicles[i][j] + " || ");
+                                }
+                                System.out.println();
+                            }
+                        }
+
+                        break;
+                    case 4:
+                        System.out.println("Has triat crear fitxa de nova reparació....");
+                        //insert code here
+                        break;
+                    case 5:
+                        System.out.println("Sortint....");
+                        break;
+                    default:
+                        System.out.println("Opció no vàlida");
+                }
+            }else{
+                System.out.println("Opció no vàlida");
+            }
+            input.nextLine();
+            System.out.println("");
+
+        }while(menuItem!=5);
+
     }
 
-    public static void AltaVehicles() {
-        //TODO: AltaVehicles
-    }
+    //insert code here
+    public static void AltaVehicle() {
+        //variables
+        if (clients[0][3] == null) {
+            System.out.println("No hi ha ningún client i per tant no es pot associar un propietari al vehicle.");
+            System.out.println("Si us plau, introdueix primer les dades de un client");
+            AltaClient();
+        } else {
+            int opcio = 1;
+            Scanner input = new Scanner(System.in);
+            while (opcio == 1) {
+                boolean jaexisteix;
+                //Introduïr les dades
+                do {
+                    System.out.println("Introdueïx la matrícula del vehicle:");
+                    String comprobacio = input.next();
+                    jaexisteix = true;
 
-    public static void AltaEmpleats() {
-        //TODO: AltaEmpleats
+                    //comprobació
+                    for (int i = 0; i < vehicles.length; i++) {
+                        if (vehicles[i][0] != null && vehicles[i][0].equals(comprobacio)) {
+                            jaexisteix = false;
+                            System.out.println("ERROR! AQUESTA MATRÍCULA JA EXISTEIX");
+                            break;
+                        }
+                    }
+                    if (jaexisteix) {
+                        vehicles[xv][0] = comprobacio;
+                        break;
+                    }
+                } while (!jaexisteix);
+                System.out.println("Introdueïx el nom de model del vehicle");
+                vehicles[xv][1] = input.next();
+                System.out.println("Intrueïx el DNI del propietari");
+                vehicles[xv][2] = input.next();
+                xv++;
+                System.out.println("Vols posar un altre vehicle?");
+                System.out.println("1. Si");
+                System.out.println("2. No");
+                opcio = input.nextInt();
+            }
+        }
     }
-
-    public static void Reparacions() {
-        //TODO: Reparacions
+    public static void AltaClient() {
     }
-
-    public static void NovaReparacions() {
-        //TODO: NovaReparacions
-    }
-
-    //TODO: Modificar
 }
-/**
- * Aqui se comentarien els mètodes
- * Test primer commit
- */
 
-}
